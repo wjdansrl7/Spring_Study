aedca492b8334037d495b9e49b12fde22559930e
wjdansrl7 <c880910@naver.com>
2023-06-06 17:04:19 +0900

    spring data jpa, 공통 인터페이스에 대한 기능 공부

---
+++ data-jpa/src/main/java/study/datajpa/repository/TeamJpaRepository.java
@@ -0,0 +1,43 @@
+package study.datajpa.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class TeamJpaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Team save(Team team) {
+        em.persist(team);
+        return team;
+    }
+
+    public void delete(Team team) {
+        em.remove(team);
+    }
+
+    public List<Team> findAll() {
+        return em.createQuery("select t from Team t", Team.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member team = em.find(Member.class, id);
+        return Optional.ofNullable(team);
+    }
+
+    public long count() {
+        return em.createQuery("select count(t) from Team t", Long.class)
+                .getSingleResult();
+    }
+
+
+}
