53c15aa6ce6d06f56dfeb462133fb086c5b6b099
wjdansrl7 <c880910@naver.com>
2023-06-08 14:21:05 +0900

    spring data jpa, 공통 인터페이스에 대한 기능 복습

---
+++ data-jpa2/src/main/java/study/datajpa2/repository/MemberJpaRepository.java
@@ -0,0 +1,47 @@
+package study.datajpa2.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import study.datajpa2.entity.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class MemberJpaRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class)
+                .getSingleResult();
+    }
+
+
+
+}
