6eff8ca1e68890dc91f239e05b60a99cb55b6eb1
wjdansrl7 <c880910@naver.com>
2023-10-29 22:01:45 +0900

    login 폼 생성 및 cookie 생성

---
+++ login/src/main/java/hello/login/domain/member/Member.java
@@ -0,0 +1,21 @@
+package hello.login.domain.member;
+
+import jdk.jfr.TransitionTo;
+import lombok.Data;
+
+import javax.validation.constraints.NotEmpty;
+
+@Data
+public class Member {
+
+    private Long id;
+
+    @NotEmpty
+    private String loginId; // 로그인 ID
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String password;
+
+
+}
